9472c6109f97f3ac47414018cdcb1b6c21b7def3
BlazingAssassin <dameplays24@gmail.com>
2020-03-02 18:27:32 -0500

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,39 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-import { Question }  from './Question.jsx';
-import firebase from 'clients/firebase.js';
+import { Question } from './Question.jsx';
+import { buildFirebase, getRandomQuestion } from '../clients/firebase.js'
 
-import {buildFirebase} from '.../clients/firebase.js'
-
-var database = buildFirebase();
-var databaseRef = database.ref('/questions');
-databaseRef.once("value").then(function(data) {
-  const questions = data.val();
-  // Do something with the questions
-});
 
 class App extends Component {
+  
   constructor(props) {
-    super(props); 
+    super(props);
+    this.state = {data: [],
+      currentQuestion: { 
+        choices: ["48", "13", "52", "50"],
+        correct_choice_index: 3,
+        question_text: "How many states are in the United States?",
+        user_id: "ZbCvDadaM0SD9YjAXrHMEDLVGoj2"
+      }
+    };
+
     var database = buildFirebase();
+    var databaseRef = database.ref("/questions");
 
-  var databaseRef = database.ref("/questions");
-  databaseRef.once("value").then(function(data) {
-    // Extract the data form the database
-    const questions = data.val();
-    // Set the state of the "data" variable to the questions data.
-    this.state = ( data: questions  ) 
+    databaseRef.once("value").then(data => {
+      // Extract the data form the database
+      const questions = data.val();
+      const currQ = getRandomQuestion(questions);
+      // Set the state of the "data" variable to the questions data.
+       this.setState({data: questions, currentQuestion: currQ });
+       console.log(currQ);
+    });
   }
-
-  
-
-  
-
-
   render() {
     return (
       <div className="app">
-        // Need to pass in the data into question
-        <Question/>
-
-        
-        
+        {/* Need to pass in the data into question*/}
+        <Question questionData={this.state.currentQuestion}/>
       </div>
     );
   }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,29 +1,37 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 
 
 
 class Question extends Component {
-  
-    constructor(props) {
-      // This question needs to accept the data passed in from the App.
-      super(props); 
-       
-        } 
-    
-      
 
-  
+  constructor(props) {
+    // This question needs to accept the data passed in from the App.
+    super(props);
+  }
+
+
+// choices: (4) ["Fortnite", "Minecraft", "Super Smash Bros. Ultimate", "Final Fantasy 7"]
+// correct_choice_index: 1
+// question_text:"Which one of these is reffered to as Indie Games?"
   render() {
     return (
       <div>
-      <h1>
-        // Use the data passed in from the App to fill out fields and render questions.
-        Hello, Students 
-       <button>Start</button> 
-        <button>Reset</button>
+        <h1>
+          {/* Use the data passed in from the App to fill out fields and render questions.*/}
+          {this.props.questionData.question_text}<br></br>
+          {this.props.questionData.choices[0]}<br></br>
+          {this.props.questionData.choices[1]}<br></br>
+          {this.props.questionData.choices[2]}<br></br>
+          {this.props.questionData.choices[3]}<br></br>
+          correct answer: {this.props.questionData.correct_choice_index}<br></br>
+
+
+          <button>Start</button>
+          <button>Reset</button>
         </h1>
       </div>
-    });
+    );
+  }
 
 }
 export { Question };
